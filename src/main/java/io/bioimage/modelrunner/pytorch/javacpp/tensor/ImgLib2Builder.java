7e63f53a6999834919d378af5797ccf938cb0173
carlosuc3m <100329787@alumnos.uc3m.es>
2023-04-11 17:21:00 +0200

    correct the imglib2 array creation

--- src/main/java/io/bioimage/modelrunner/pytorch/javacpp/tensor/ImgLib2Builder.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/javacpp/tensor/ImgLib2Builder.java
@@ -30,11 +30,11 @@ import org.bytedeco.pytorch.Tensor;
 import io.bioimage.modelrunner.utils.IndexingUtils;
 import net.imglib2.Cursor;
 import net.imglib2.img.Img;
-import net.imglib2.img.ImgFactory;
-import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 
@@ -62,8 +62,7 @@ public class ImgLib2Builder {
     	} else if (tensor.dtype().isScalarType(org.bytedeco.pytorch.global.torch.ScalarType.Double)) {
     		return (Img<T>) buildFromTensorDouble(tensor);
     	} else if (tensor.dtype().isScalarType(org.bytedeco.pytorch.global.torch.ScalarType.Long)) {
-    		// TODO
-            return (Img<T>) buildFromTensorDouble(tensor);
+            return (Img<T>) buildFromTensorLong(tensor);
     	} else {
             throw new IllegalArgumentException("Unsupported tensor type: " + tensor.scalar_type());
     	}
@@ -79,10 +78,13 @@ public class ImgLib2Builder {
     private static Img<ByteType> buildFromTensorByte(org.bytedeco.pytorch.Tensor tensor)
     {
     	long[] tensorShape = tensor.shape();
-    	final ImgFactory< ByteType > factory = new CellImgFactory<>( new ByteType(), 5 );
+    	final ArrayImgFactory< ByteType > factory = new ArrayImgFactory<>( new ByteType() );
         final Img< ByteType > outputImg = (Img<ByteType>) factory.create(tensorShape);
     	Cursor<ByteType> tensorCursor= outputImg.cursor();
-		byte[] flatArr = tensor.asByteBuffer().array();
+    	long flatSize = 1;
+    	for (long l : tensorShape) {flatSize *= l;}
+    	byte[] flatArr = new byte[(int) flatSize];
+    	tensor.data_ptr_byte().get(flatArr);
 		while (tensorCursor.hasNext()) {
 			tensorCursor.fwd();
 			long[] cursorPos = tensorCursor.positionAsLongArray();
@@ -103,10 +105,13 @@ public class ImgLib2Builder {
     private static Img<IntType> buildFromTensorInt(org.bytedeco.pytorch.Tensor tensor)
     {
     	long[] tensorShape = tensor.shape();
-    	final ImgFactory< IntType > factory = new CellImgFactory<>( new IntType(), 5 );
+    	final ArrayImgFactory< IntType > factory = new ArrayImgFactory<>( new IntType() );
         final Img< IntType > outputImg = (Img<IntType>) factory.create(tensorShape);
     	Cursor<IntType> tensorCursor= outputImg.cursor();
-		int[] flatArr = tensor.asByteBuffer().asIntBuffer().array();
+    	long flatSize = 1;
+    	for (long l : tensorShape) {flatSize *= l;}
+    	int[] flatArr = new int[(int) flatSize];
+    	tensor.data_ptr_int().get(flatArr);
 		while (tensorCursor.hasNext()) {
 			tensorCursor.fwd();
 			long[] cursorPos = tensorCursor.positionAsLongArray();
@@ -127,7 +132,7 @@ public class ImgLib2Builder {
     private static Img<FloatType> buildFromTensorFloat(org.bytedeco.pytorch.Tensor tensor)
     {
     	long[] tensorShape = tensor.shape();
-    	final ImgFactory< FloatType > factory = new CellImgFactory<>( new FloatType(), 5 );
+    	final ArrayImgFactory< FloatType > factory = new ArrayImgFactory<>( new FloatType() );
         final Img< FloatType > outputImg = (Img<FloatType>) factory.create(tensorShape);
     	Cursor<FloatType> tensorCursor= outputImg.cursor();
     	long flatSize = 1;
@@ -154,15 +159,45 @@ public class ImgLib2Builder {
     private static Img<DoubleType> buildFromTensorDouble(org.bytedeco.pytorch.Tensor tensor)
     {
     	long[] tensorShape = tensor.shape();
-    	final ImgFactory< DoubleType > factory = new CellImgFactory<>( new DoubleType(), 5 );
+    	final ArrayImgFactory< DoubleType > factory = new ArrayImgFactory<>( new DoubleType() );
         final Img< DoubleType > outputImg = (Img<DoubleType>) factory.create(tensorShape);
     	Cursor<DoubleType> tensorCursor= outputImg.cursor();
-		double[] flatArr = tensor.asByteBuffer().asDoubleBuffer().array();
+    	long flatSize = 1;
+    	for (long l : tensorShape) {flatSize *= l;}
+    	double[] flatArr = new double[(int) flatSize];
+    	tensor.data_ptr_double().get(flatArr);
 		while (tensorCursor.hasNext()) {
 			tensorCursor.fwd();
 			long[] cursorPos = tensorCursor.positionAsLongArray();
         	int flatPos = IndexingUtils.multidimensionalIntoFlatIndex(cursorPos, tensorShape);
         	double val = flatArr[flatPos];
+        	tensorCursor.get().set(val);
+		}
+	 	return outputImg;
+    }
+
+    /**
+     * Builds a {@link Img} from a unsigned double-typed {@link NDArray}.
+     * 
+     * @param tensor
+     *        The tensor data is read from.
+     * @return The INDArray built from the tensor of type {@link DataType#DOUBLE}.
+     */
+    private static Img<LongType> buildFromTensorLong(org.bytedeco.pytorch.Tensor tensor)
+    {
+    	long[] tensorShape = tensor.shape();
+    	final ArrayImgFactory< LongType > factory = new ArrayImgFactory<>( new LongType() );
+        final Img< LongType > outputImg = (Img<LongType>) factory.create(tensorShape);
+    	Cursor<LongType> tensorCursor= outputImg.cursor();
+    	long flatSize = 1;
+    	for (long l : tensorShape) {flatSize *= l;}
+    	long[] flatArr = new long[(int) flatSize];
+    	tensor.data_ptr_long().get(flatArr);
+		while (tensorCursor.hasNext()) {
+			tensorCursor.fwd();
+			long[] cursorPos = tensorCursor.positionAsLongArray();
+        	int flatPos = IndexingUtils.multidimensionalIntoFlatIndex(cursorPos, tensorShape);
+        	long val = flatArr[flatPos];
         	tensorCursor.get().set(val);
 		}
 	 	return outputImg;
