bec587c20bd94c7c41438ebcb198ed4accc0fef8
carlosuc3m <100329787@alumnos.uc3m.es>
2023-04-11 11:51:31 +0200

    add docs to main class

--- src/main/java/io/bioimage/modelrunner/pytorch/javacpp/PytorchJavaCPPInterface.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/javacpp/PytorchJavaCPPInterface.java
@@ -25,8 +25,6 @@
  */
 package io.bioimage.modelrunner.pytorch.javacpp;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,21 +40,42 @@ import io.bioimage.modelrunner.pytorch.javacpp.tensor.ImgLib2Builder;
 import io.bioimage.modelrunner.pytorch.javacpp.tensor.JavaCPPTensorBuilder;
 import io.bioimage.modelrunner.tensor.Tensor;
 
-
 /**
- * This class implements an interface that allows the main plugin
- * to interact in an agnostic way with the Deep Learning model and
- * tensors to make inference.
- * This implementation add the Pytorch support to the main program.
+ * This class implements an interface that allows the main plugin to interact in
+ * an agnostic way with the Deep Learning model and tensors to make inference.
+ * This implementation adds Pytorch support to the main program using the JavaCPP
+ * library to load the corresponding native bindings.
  * 
- * @see SequenceBuilder SequenceBuilder: Create sequences from tensors.
- * @see JavaCPPTensorBuilder TensorBuilder: Create tensors from images and sequences.
- * @author Carlos Garcia Lopez de Haro 
+ * Class to that communicates with the dl-model runner, 
+ * @see <a href="https://github.com/bioimage-io/model-runner-java">dlmodelrunner</a>
+ * to execute Pytorch models.
+ * This class implements the interface {@link DeepLearningEngineInterface} to get the 
+ * agnostic {@link io.bioimage.modelrunner.tensor.Tensor}, convert them into 
+ * {@link org.bytedeco.pytorch.Tensor}, execute a Pytorch Deep Learning model on them and
+ * convert the results back to {@link io.bioimage.modelrunner.tensor.Tensor} to send them 
+ * to the main program in an agnostic manner.
+ * 
+ * {@link ImgLib2Builder}. Creates ImgLib2 images for the backend
+ *  of {@link io.bioimage.modelrunner.tensor.Tensor} from {@link org.bytedeco.pytorch.Tensor}
+ * {@link JavaCPPTensorBuilder}. Converts {@link io.bioimage.modelrunner.tensor.Tensor} 
+ *  into {@link org.bytedeco.pytorch.Tensor}
+ *  
+ * 
+ * @author Carlos Garcia Lopez de Haro
  */
 public class PytorchJavaCPPInterface implements DeepLearningEngineInterface
 {
+
+	/**
+	 * The Pytorch torchscript model loaded with JavaCpp
+	 */
 	private JitModule model;
-    
+
+	/**
+	 * Constructor for the interface. It is going to be called from the 
+	 * dlmodel-runner.
+	 * This one tries to use mkl to be faster
+	 */
     public PytorchJavaCPPInterface()
     {
         System.setProperty("org.bytedeco.openblas.load", "mkl");
@@ -65,6 +84,13 @@ public class PytorchJavaCPPInterface implements DeepLearningEngineInterface
     public static void main(String[] args) {
     }
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * Run a Pytorch model using JavaCpp on the data provided by the {@link Tensor} input list
+	 * and modifies the output list with the results obtained
+	 * 
+	 */
 	@Override
 	public void run(List<Tensor<?>> inputTensors, List<Tensor<?>> outputTensors) throws RunModelException {
 		IValueVector inputsVector = new IValueVector();
@@ -90,6 +116,11 @@ public class PytorchJavaCPPInterface implements DeepLearningEngineInterface
 		inputsVector.deallocate();		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * 
+     * Load a Pytorch torchscript model using JavaCpp. 
+	 */
 	@Override
 	public void loadModel(String modelFolder, String modelSource) throws LoadModelException {
     	try {
@@ -100,6 +131,13 @@ public class PytorchJavaCPPInterface implements DeepLearningEngineInterface
     	}
 	}
 
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * Closes the Pytorch model and sets it to null once the model is not needed anymore.
+	 * 
+	 */
 	@Override
 	public void closeModel() {
 		if (model == null)
@@ -107,79 +145,26 @@ public class PytorchJavaCPPInterface implements DeepLearningEngineInterface
 		model.close();
 		model.deallocate();
 	}
-	
+
 	/**
-	 * Create the list a list of output tensors agnostic to the Deep Learning engine
-	 * that can be readable by Deep Icy
-	 * @param outputTensors
-	 * 	an NDList containing NDArrays (tensors)
-	 * @param outputTensors2
-	 * 	the names given to the tensors by the model
-	 * @return a list with Deep Learning framework agnostic tensors
-	 * @throws RunModelException If the number of tensors expected is not the same as the number of
-	 * 	Tensors outputed by the model
-	 */
-	public static void fillOutputTensors(TensorVector outputNDArrays, List<Tensor<?>> outputTensors) throws RunModelException{
-		if (outputNDArrays.size() != outputTensors.size())
-			throw new RunModelException((int) outputNDArrays.size(), outputTensors.size());
-		for (int i = 0; i < outputNDArrays.size(); i ++) {
-			outputTensors.get(i).setData(ImgLib2Builder.build(outputNDArrays.get(i)));
-			outputNDArrays.get(i).close();
-			outputNDArrays.get(i).deallocate();
-		}
-	}
-	
-	/**
-	 * Print the correct message depending on the exception produced when
-	 * trying to load the model
+	 * Create the list a list of output tensors agnostic to the Deep Learning
+	 * engine that can be readable by the model-runner
 	 * 
-	 * @param ex
-	 * 	the exception that occurred
+	 * @param tensorVector 
+	 * 	an object containing a list of Pytorch JavaCpp tensors
+	 * @param outputTensors 
+	 * 	the list of output tensors where the output data is going to be written to send back
+	 * 	to the model runner
+	 * @throws RunModelException If the number of tensors expected is not the same
+	 *           as the number of Tensors outputed by the model
 	 */
-	public static void managePytorchExceptions(Exception e) {
-		if (e instanceof MalformedURLException) {
-			System.out.println("No model was found in the folder provided.");
-		} else if (e instanceof Exception) {
-			String err = e.getMessage();
-			String os = System.getProperty("os.name").toLowerCase();
-			String msg;
-			if (os.contains("win") && err.contains("https://github.com/awslabs/djl/blob/master/docs/development/troubleshooting.md")) {
-				msg = "DeepIcy could not load the model.\n" + 
-					"Please install the Visual Studio 2019 redistributables and reboot" +
-					"your machine to be able to use Pytorch with DeepIcy.\n" +
-					"For more information:\n" +
-					" -https://github.com/awslabs/djl/blob/master/docs/development/troubleshooting.md\n" +
-					" -https://github.com/awslabs/djl/issues/126\n" +
-					"If you already have installed VS2019 redistributables, the error" +
-					"might be caused by a missing dependency or an incompatible Pytorch version.\n" + 
-					"Furthermore, the DJL Pytorch dependencies (pytorch-egine, pytorch-api and pytorch-native-auto).\n" +
-					"should be compatible with each other." +
-					"Please check the DeepIcy Wiki.";
-			} else if((os.contains("linux") || os.contains("unix")) && err.contains("https://github.com/awslabs/djl/blob/master/docs/development/troubleshooting.md")){
-				msg  = "DeepIcy could not load the model.\n" +
-					"Check that there are no repeated dependencies on the jars folder.\n" +
-					"The problem might be caused by a missing or repeated dependency or an incompatible Pytorch version.\n" +
-					"Furthermore, the DJL Pytorch dependencies (pytorch-egine, pytorch-api and pytorch-native-auto) " +
-					"should be compatible with each other.\n" +
-					"If the problem persists, please check the DeepIcy Wiki.";
-			} else {
-				msg  = "DeepIcy could not load the model.\n" +
-					"Either the DJL Pytorch version is incompatible with the Torchscript model's " +
-					"Pytorch version or the DJL Pytorch dependencies (pytorch-egine, pytorch-api and pytorch-native-auto) " + 
-					"are not compatible with each other.\n" +
-					"Please check the DeepIcy Wiki.";
-			}
-			System.out.println(msg);
-		} else if (e instanceof IOException) {
-			String msg = "DeepImageJ could not load the model.\n" + 
-				"The model provided is not a correct Torchscript model.";
-			System.out.println(msg);
-		} else if (e instanceof IOException) {
-			System.out.println("An error occurred accessing the model file.");
+	public static void fillOutputTensors(TensorVector tensorVector, List<Tensor<?>> outputTensors) throws RunModelException{
+		if (tensorVector.size() != outputTensors.size())
+			throw new RunModelException((int) tensorVector.size(), outputTensors.size());
+		for (int i = 0; i < tensorVector.size(); i ++) {
+			outputTensors.get(i).setData(ImgLib2Builder.build(tensorVector.get(i)));
+			tensorVector.get(i).close();
+			tensorVector.get(i).deallocate();
 		}
-	}
-	
-	public void finalize() {
-		System.out.println("Collected Garbage");
 	}
 }
