d951617c73db5b586303c512d4cba5b1700c002d
carlosuc3m <100329787@alumnos.uc3m.es>
2024-11-23 12:32:35 +0100

    remove changes

--- src/main/java/io/bioimage/modelrunner/pytorch/javacpp/shm/ShmBuilder.java
+++ src/main/java/io/bioimage/modelrunner/pytorch/javacpp/shm/ShmBuilder.java
@@ -103,18 +103,6 @@ public final class ShmBuilder
 					+ " is too big. Max number of elements per float output tensor supported: " + Integer.MAX_VALUE / 4);
 		SharedMemoryArray shma = SharedMemoryArray.readOrCreate(memoryName, arrayShape, new UnsignedByteType(), false, true);
         shma.getDataBufferNoHeader().put(tensor.asByteBuffer());
-        shma.getDataBufferNoHeader().rewind();
-        tensor.asByteBuffer().rewind();
-		float sum = 0;
-		while (tensor.asByteBuffer().hasRemaining())
-			sum += tensor.asByteBuffer().get();
-		float sum2 = 0;
-		while (shma.getDataBufferNoHeader().hasRemaining())
-			sum2 += shma.getDataBufferNoHeader().get();
-		System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK  SRC  " + sum);
-		System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK  TAR  " + sum2);
-        shma.getDataBufferNoHeader().rewind();
-        tensor.asByteBuffer().rewind();
         if (PlatformDetection.isWindows()) shma.close();
     }
 
